62d2fb3c1fcbe4f89f912d4c5d5251f6e8f44b53
Jackson-Vieira <jacksonvieiira12@gmail.com>
2024-04-01 03:33:17 -0300

    fix seo

--- app.vue
+++ app.vue
@@ -4,10 +4,10 @@ useHead({
 })
 
 useSeoMeta({
-  title: 'PixMeBio - Compartilhamento Seguro e Elegante do Seu PIX de forma fácil',
-  ogTitle: 'PixMeBio - Compartilhamento Seguro e Elegante do Seu PIX de forma fácil',
-  description: 'Otimize sua presença online com o PixMeBio, um app de perfil social e QR code que oferece compartilhamento seguro e elegante do seu PIX de forma fácil.',
-  ogDescription: 'Otimize sua presença online com o PixMeBio, um app de perfil social e QR code que oferece compartilhamento seguro e elegante do seu PIX de forma fácil'
+  title: 'Pixmebio - Compartilhamento Seguro e Elegante do Seu PIX de forma fácil',
+  ogTitle: 'Pixmebio - Compartilhamento Seguro e Elegante do Seu PIX de forma fácil',
+  description: 'Otimize sua presença online com o Pixmebio, um app de perfil social e QR code que oferece compartilhamento seguro e elegante do seu PIX de forma fácil.',
+  ogDescription: 'Otimize sua presença online com o Pixmebio, um app de perfil social e QR code que oferece compartilhamento seguro e elegante do seu PIX de forma fácil'
 })
 
 </script>
